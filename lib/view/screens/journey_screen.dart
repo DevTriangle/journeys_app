56d1eecdab350a035426ef8800dcd792fa462d2a
thego <thegoshagames@gmail.com>
2023-05-26 13:15:46 +0300

    ♦

--- lib/view/screens/journey_screen.dart
+++ lib/view/screens/journey_screen.dart
@@ -105,125 +105,133 @@ class JourneyScreenState extends State<JourneyScreen> {
             future: _getJourneys,
             builder: (context, snapshot) {
               if (snapshot.hasData) {
-                return Column(
-                  children: [
-                    Expanded(
-                      child: ListView.builder(
-                        itemCount: snapshot.data!.length,
-                        itemBuilder: ((context, index) {
-                          return AppJourneyCard(
-                            margin: EdgeInsets.only(bottom: 4),
-                            journey: snapshot.data![index],
-                            onChanged: (list) async {
-                              viewModel.journeys[index].items = list;
-                              await viewModel.saveJourneys();
-                              setState(() {});
-                            },
-                            onTap: () {
-                              if (currentJourney == snapshot.data![index]) {
-                                currentJourney = null;
-                              } else {
-                                currentJourney = snapshot.data![index];
-                              }
+                if (snapshot.data!.isNotEmpty) {
+                  return Column(
+                    children: [
+                      Expanded(
+                        child: ListView.builder(
+                          itemCount: snapshot.data!.length,
+                          itemBuilder: ((context, index) {
+                            return AppJourneyCard(
+                              margin: EdgeInsets.only(bottom: 4),
+                              journey: snapshot.data![index],
+                              onChanged: (list) async {
+                                viewModel.journeys[index].items = list;
+                                await viewModel.saveJourneys();
+                                setState(() {});
+                              },
+                              onTap: () {
+                                if (currentJourney == snapshot.data![index]) {
+                                  currentJourney = null;
+                                } else {
+                                  currentJourney = snapshot.data![index];
+                                }
 
-                              setState(() {});
-                            },
-                            isExpanded: currentJourney == snapshot.data![index],
-                            onAddTap: () {
-                              _actionValue = snapshot.data![index].actions[0];
+                                setState(() {});
+                              },
+                              isExpanded: currentJourney == snapshot.data![index],
+                              onAddTap: () {
+                                _actionValue = snapshot.data![index].actions[0];
 
-                              showDialog(
-                                context: context,
-                                builder: (builder) {
-                                  return Dialog(
-                                    backgroundColor: AppColors.backgroundColor,
-                                    child: StatefulBuilder(builder: (context, setDialogState) {
-                                      return Wrap(
-                                        children: [
-                                          Padding(
-                                            padding: const EdgeInsets.all(16),
-                                            child: Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
-                                              Container(
-                                                padding: EdgeInsets.only(bottom: 6),
-                                                child: Text(
-                                                  "Действие",
-                                                  style: TextStyle(color: AppColors.hintColor, fontSize: 14),
-                                                ),
-                                              ),
-                                              Container(
-                                                padding: EdgeInsets.symmetric(horizontal: 16, vertical: 2),
-                                                decoration: BoxDecoration(color: Colors.white),
-                                                child: DropdownButtonHideUnderline(
-                                                  child: DropdownButton<String>(
-                                                    dropdownColor: Colors.white,
-                                                    items: snapshot.data![index].actions.map((String item) {
-                                                      return DropdownMenuItem<String>(value: item, child: Text(item));
-                                                    }).toList(),
-                                                    value: _actionValue,
-                                                    isExpanded: true,
-                                                    style: TextStyle(
-                                                      color: Colors.black,
-                                                      fontWeight: FontWeight.w500,
-                                                      fontSize: 16,
-                                                    ),
-                                                    onChanged: (value) {
-                                                      _actionValue = value!;
-                                                      setDialogState(() {});
-                                                    },
+                                showDialog(
+                                  context: context,
+                                  builder: (builder) {
+                                    return Dialog(
+                                      backgroundColor: AppColors.backgroundColor,
+                                      child: StatefulBuilder(builder: (context, setDialogState) {
+                                        return Wrap(
+                                          children: [
+                                            Padding(
+                                              padding: const EdgeInsets.all(16),
+                                              child: Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
+                                                Container(
+                                                  padding: EdgeInsets.only(bottom: 6),
+                                                  child: Text(
+                                                    "Действие",
+                                                    style: TextStyle(color: AppColors.hintColor, fontSize: 14),
                                                   ),
                                                 ),
-                                              ),
-                                              Container(
-                                                padding: EdgeInsets.only(bottom: 6, top: 6),
-                                                child: Text(
-                                                  "Название предмета",
-                                                  style: TextStyle(color: AppColors.hintColor, fontSize: 14),
-                                                ),
-                                              ),
-                                              AppTextField(
-                                                hint: "",
-                                                onChanged: (value) {
-                                                  _itemName = value;
-                                                },
-                                              ),
-                                              Row(
-                                                mainAxisAlignment: MainAxisAlignment.end,
-                                                children: [
-                                                  TextButton(
-                                                    onPressed: () async {
-                                                      if (_itemName.trim().isNotEmpty) {
-                                                        viewModel.journeys[index].items.add(JourneyItem(_actionValue, _itemName, 0));
-                                                        await viewModel.saveJourneys();
-
-                                                        setState(() {});
-
-                                                        Navigator.pushReplacement(context, MaterialPageRoute(builder: (builder) => HomeScreen()));
-                                                      } else {}
-                                                    },
-                                                    child: Padding(
-                                                      padding: const EdgeInsets.symmetric(horizontal: 12, vertical: 6),
-                                                      child: Text("Добавить"),
+                                                Container(
+                                                  padding: EdgeInsets.symmetric(horizontal: 16, vertical: 2),
+                                                  decoration: BoxDecoration(color: Colors.white),
+                                                  child: DropdownButtonHideUnderline(
+                                                    child: DropdownButton<String>(
+                                                      dropdownColor: Colors.white,
+                                                      items: snapshot.data![index].actions.map((String item) {
+                                                        return DropdownMenuItem<String>(value: item, child: Text(item));
+                                                      }).toList(),
+                                                      value: _actionValue,
+                                                      isExpanded: true,
+                                                      style: TextStyle(
+                                                        color: Colors.black,
+                                                        fontWeight: FontWeight.w500,
+                                                        fontSize: 16,
+                                                      ),
+                                                      onChanged: (value) {
+                                                        _actionValue = value!;
+                                                        setDialogState(() {});
+                                                      },
                                                     ),
                                                   ),
-                                                ],
-                                              ),
-                                            ]),
-                                          ),
-                                        ],
-                                      );
-                                    }),
-                                  );
-                                },
-                              );
-                            },
-                          );
-                        }),
-                      ),
-                    )
-                  ],
-                );
+                                                ),
+                                                Container(
+                                                  padding: EdgeInsets.only(bottom: 6, top: 6),
+                                                  child: Text(
+                                                    "Название предмета",
+                                                    style: TextStyle(color: AppColors.hintColor, fontSize: 14),
+                                                  ),
+                                                ),
+                                                AppTextField(
+                                                  hint: "",
+                                                  onChanged: (value) {
+                                                    _itemName = value;
+                                                  },
+                                                ),
+                                                Row(
+                                                  mainAxisAlignment: MainAxisAlignment.end,
+                                                  children: [
+                                                    TextButton(
+                                                      onPressed: () async {
+                                                        if (_itemName.trim().isNotEmpty) {
+                                                          viewModel.journeys[index].items.add(JourneyItem(_actionValue, _itemName, 0));
+                                                          await viewModel.saveJourneys();
+
+                                                          setState(() {});
+
+                                                          Navigator.pushReplacement(context, MaterialPageRoute(builder: (builder) => HomeScreen()));
+                                                        } else {}
+                                                      },
+                                                      child: Padding(
+                                                        padding: const EdgeInsets.symmetric(horizontal: 12, vertical: 6),
+                                                        child: Text("Добавить"),
+                                                      ),
+                                                    ),
+                                                  ],
+                                                ),
+                                              ]),
+                                            ),
+                                          ],
+                                        );
+                                      }),
+                                    );
+                                  },
+                                );
+                              },
+                            );
+                          }),
+                        ),
+                      )
+                    ],
+                  );
+                } else {
+                  return Center(
+                    child: Text("Путешествий нет!"),
+                  );
+                }
               } else {
-                return SizedBox();
+                return Center(
+                  child: Text("Путешествий нет!"),
+                );
               }
             }),
       ),
